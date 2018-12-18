1c5a09f6a1174b1643a53e75715797c0de12c367
Makan Dey <makan@mastacoder.com>
2018-12-17 21:14:07 -0500

    Oops
    I was using this for testing.

--- models/configuration.php
+++ models/configuration.php
@@ -1,9 +1,9 @@
 <?php
 
 $_GLOBAL = array(
-    'client' => "37c9b2d96d0d47ebb147ef163020e7f9",
-    'secret' => "c6a5c0f2b2224b1c8b83b6df0de4b218",
-    'redirect' => "http://localhost/mySpotify/auth.php"
+    'client' => "",
+    'secret' => "",
+    'redirect' => ""
 )
 
 ?>
