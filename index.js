c8a8eb1a26020ce4447e150df0fe5fb25e318ad3
Makan Dey <makan@mastacoder.com>
2019-04-20 03:20:27 -0400

    this is for you kevin <3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require("http");
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var querystring = require('querystring');
 var Mongo = require('connect-mongo')(session);
 
 var app = express();
@@ -10,28 +11,32 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'twig');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(require('stylus').middleware({
+    src: __dirname + '/public'
+}));
 app.use(express.static(__dirname + '/public'));
 
 process.env.DB_HOST = process.env.DB_HOST || 'localhost'
 process.env.DB_PORT = process.env.DB_PORT || 27017;
 process.env.DB_NAME = process.env.DB_NAME || 'myspotify';
 
-process.env.DB_URL = 'mongodb://'+process.env.DB_HOST+':'+process.env.DB_PORT;
+process.env.DB_URL = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT;
 
 app.use(session({
     secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
     proxy: true,
     resave: true,
     saveUninitialized: true,
-    store: new Mongo({ url: process.env.DB_URL })
+    store: new Mongo({
+        url: process.env.DB_URL
     })
-);
+}));
 
-app.sethgay = "nou";
-require('./routes.js')(app);
+require('./routes.js')(app, querystring);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
     console.log('[MySpotify] Listening on Port ' + app.get('port'));
 });
--- routes.js
+++ routes.js
@@ -1,5 +1,56 @@
-module.exports = function(app) {    
+require('./config.js');
+
+var spotifyApi = require('spotify-web-api-node');
+
+module.exports = function(app, querystring) {
     app.get('/', function(req, res) {
-        res.render('index.twig', { title: app.sethgay })
+        res.render('index.twig', {
+            title: app.sethgay
+        })
+    });
+
+    app.get('/login', function(req, res) {
+        var state = generateRandomString(16);
+        res.cookie(stateKey, state);
+
+        // your application requests authorization
+        var scope = 'user-read-private user-read-email';
+        res.redirect('https://accounts.spotify.com/authorize?' + querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+    });
+
+    app.get('/callback', function(req, res) {
+        // The code that's returned as a query parameter to the redirect URI
+        var code = 'MQCbtKe23z7YzzS44KzZzZgjQa621hgSzHN';
+        
+        // Retrieve an access token and a refresh token
+        spotifyApi.authorizationCodeGrant(code).then(
+        function(data) {
+            console.log('The token expires in ' + data.body['expires_in']);
+            console.log('The access token is ' + data.body['access_token']);
+            console.log('The refresh token is ' + data.body['refresh_token']);
+        
+            // Set the access token on the API object to use it in later calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+            spotifyApi.setRefreshToken(data.body['refresh_token']);
+        },
+        function(err) {
+            console.log('Something went wrong!', err);
+        });
     });
 }
+
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
