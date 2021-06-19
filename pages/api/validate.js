8b76703650cd8184b08dfa3fb5f059951c315a34
Makan D <makan@makan.bz>
2021-06-19 19:46:22 -0400

    bug please.

--- pages/api/validate.js
+++ pages/api/validate.js
@@ -8,7 +8,7 @@ export default async function validateAPI(req, res) {
     res.status(200).json({
       'id': resBody.id,
       'name': resBody.display_name,
-      'image': resBody.images[0].url || ''
+      'image': resBody.images.length === 0 ? '' : resBody.images[0].url
     });
   }, () => {
     res.status(401).json("Invalid token");
