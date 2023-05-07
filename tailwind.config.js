929c52e4d0cf57e8d3b30144fbdc4c2d198e4015
MastaCoder <makan@makan.bz>
2023-05-06 22:08:10 -0400

    chore: next.js base setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    fontFamily: {
-      'display': ['Manrope', 'sans-serif'],
-      'body': ['Roboto', 'sans-serif']
-    },
     extend: {
-      translate: {
-        double: "200%",
-        triple: "300%",
-        quad: "400%"
-      }
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
