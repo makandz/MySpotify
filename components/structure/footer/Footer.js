5d7346a715737b4f4f87c81ef90e631f761dfe8b
Makan <makan@makan.bz>
2025-10-03 16:50:59 -0400

    feat: start fresh

--- components/structure/footer/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="mt-auto bg-black text-center p-4 text-gray-500">
-      <span>All assets related to artists, albums, and songs are retrieved from Spotify. This website is not affiliated
-        with Spotify.</span><br />
-    </div>
-  )
-}
